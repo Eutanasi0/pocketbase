c28b45431804376dc7b1eca530b1ddd6ba0c7fb9
Eutanasi0 <benjaminflores4e@gmail.com>
2024-06-26 21:54:12 -0500

    update gitignore

--- pb_hooks/postplans.pb.js
+++ pb_hooks/postplans.pb.js
@@ -1,29 +1,38 @@
 
+
 routerAdd("POST", "/plans", (c) => {
   const model = new DynamicModel({
-    name: "",
+    title: "",
     description: "",
-    start: "",
-    estimated_end: "",
-    userId: "",
-    data: {}
+    startDate: "",
+    clients: []
   });
 
   c.bind(model);
 
- 
-  if (!model.name || !model.start || !model.userId || !model.estimated_end || !model.data || !model.description) {
+  // Validar los campos obligatorios
+  if (!model.title || !model.startDate || !model.clients || !model.description) {
     c.noContent(400);
     return;
   }
 
+  // Transformar los datos de los clientes para ajustarlos al modelo esperado
+  const transformedClients = model.clients.map(client => ({
+    id: client.id,
+    description: client.description,
+    products: client.products.map(product => ({
+      id: product.id,
+      name: product.name,
+      amount_requested: product.amount_requested
+    }))
+  }));
+
+  // Crear un nuevo registro para la colección 'plans'
   const newPlan = new Record("plans", {
-    name: model.name,
+    title: model.title,
     description: model.description,
-    start: model.start,
-    estimated_end: model.estimated_end,
-    userId: model.userId,
-    data: model.data
+    startDate: model.startDate,
+    clients: transformedClients
   });
 
   // Guardar el nuevo plan en la colección 'plans'
@@ -31,8 +40,6 @@ routerAdd("POST", "/plans", (c) => {
 
   return c.json(201, { success: true, id: newPlan.id });
 });
-
-
 
 
 
