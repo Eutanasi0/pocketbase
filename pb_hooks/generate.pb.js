7e437d2761c2700d5c7d3f545e196a6f85891a78
feelware <stevanpersonal@gmail.com>
2024-06-28 19:34:18 -0500

    feat: log request body

--- pb_hooks/generate.pb.js
+++ pb_hooks/generate.pb.js
@@ -1,5 +1,6 @@
 routerAdd("POST", "/generate", (c) => {
   const data = $apis.requestInfo(c).data
-  console.log(data)
+  console.log(JSON.toString(data))
+  $app.logger().info(JSON.toString(data))
   return c.json(200)
 });
