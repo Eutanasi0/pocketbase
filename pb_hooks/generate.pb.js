fbadb4f6b28fac886a695457c25551726a606751
feelware <stevanpersonal@gmail.com>
2024-06-29 01:16:33 -0500

    feat: request distance matrix

--- pb_hooks/generate.pb.js
+++ pb_hooks/generate.pb.js
@@ -19,7 +19,7 @@ routerAdd('POST', '/generate', (c) => {
   //   &destinations=${addresses}
   //   &key=${API_KEY}`
 
-  debug(process.env.USER)
+  debug(process.env.APIKEY)
 
   // const title = body.title
   // const description = body.description
