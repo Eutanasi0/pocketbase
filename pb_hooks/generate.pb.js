2fb9939268775a42770569e9c6417120c34e2be8
feelware <stevanpersonal@gmail.com>
2024-06-29 17:28:08 -0500

    feat: generate routes (wip)

--- pb_hooks/generate.pb.js
+++ pb_hooks/generate.pb.js
@@ -53,27 +53,45 @@ routerAdd('POST', '/generate', (c) => {
     vehicles[0].get('capacity')
   )
 
+  const routes = []
+
   for (const [route_index, raw_route] of raw_plan.entries()) {
-    $app.logger().debug(`waypoint 0 of route ${route_index}`,  
+    const route_clients = []
+    const route_segments = []
+    const first_client = clients[raw_route[1] - 1]
+    const first_segment_distance = distance_matrix[0][raw_route[1]]
+    const first_segment_time = res.json.rows[0].elements[raw_route[1]].duration.value
+    $app.logger().debug(`segment 0 of route ${route_index}`,  
       'from (depot)', depot.get('formatted_address'),
-      'to', clients[raw_route[1] - 1].formatted_address,
-      'distance', distance_matrix[0][raw_route[1]],
-      'time', res.json.rows[0].elements[raw_route[1]].duration.value
+      'to', first_client.formatted_address,
+      'distance', first_segment_distance,
+      'time', first_segment_time
     )
     for (let w_index = 1; w_index < raw_route.length - 2; w_index++) {
-      $app.logger().debug(`waypoint ${w_index} of route ${route_index}`,  
-        'from', clients[raw_route[w_index] - 1].formatted_address, 
-        'to', clients[raw_route[w_index + 1] - 1].formatted_address,
-        'distance', distance_matrix[raw_route[w_index]][raw_route[w_index + 1]],
-        'time', res.json.rows[raw_route[w_index]].elements[raw_route[w_index + 1]].duration.value
+      const client_from = clients[raw_route[w_index] - 1]
+      const client_to = clients[raw_route[w_index + 1] - 1]
+      const distance = distance_matrix[raw_route[w_index]][raw_route[w_index + 1]]
+      const time = res.json.rows[raw_route[w_index]].elements[raw_route[w_index + 1]].duration.value
+
+      $app.logger().debug(`segment ${w_index} of route ${route_index}`,  
+        'from', client_from.formatted_address, 
+        'to', client_to.formatted_address,
+        'distance', distance,
+        'time', time
       )
     }
-    $app.logger().debug(`waypoint ${raw_route.length - 2} of route ${route_index}`,   
-      'from', clients[raw_route[raw_route.length - 2] - 1].formatted_address,  
+    const last_client = clients[raw_route[raw_route.length - 2] - 1]
+    const last_segment_distance = distance_matrix[raw_route[raw_route.length - 2]][0]
+    const last_segment_time = res.json.rows[raw_route[raw_route.length - 2]].elements[0].duration.value
+    $app.logger().debug(`segment ${raw_route.length - 2} of route ${route_index}`,   
+      'from', last_client.formatted_address,  
       'to (depot)', depot.get('formatted_address'),
-      'distance', distance_matrix[raw_route[raw_route.length - 2]][0],
-      'time', res.json.rows[raw_route[raw_route.length - 2]].elements[0].duration.value
+      'distance', last_segment_distance,
+      'time', last_segment_time
     )
+    const route = {
+
+    }
   }
 
   // const title = body.title
